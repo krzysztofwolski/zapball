e614c41e766eb9975522beb045003cc61b9856d0
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2018-02-07 17:59:08 +0100

    Add default secret key

--- zapball/settings.py
+++ zapball/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'you_totally_should_change_me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
